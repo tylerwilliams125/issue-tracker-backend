f9e76645861bdb37019ada496d00db34bbd3bbe0
tylerwilliams125 <Tyler_Williams@insideranken.org>
2023-09-14 00:18:45 -0500

    Lab 01-02

--- routes/api/bug.js
+++ routes/api/bug.js
@@ -8,9 +8,9 @@ const debugBug = debug ('app.BugRouter');
 router.use(express.urlencoded({extended:false}));
 
 const bugsArray = [
-    {"title":"Bonzi Buddy","description":"Bonzi Buddy will pop up and sing Chanel","stepsToReproduce":"see on both sides like chanel", "_id":1},
-    {"title":"Bugs Bunny","description":"Bugs will put a pipe bomb in your mailbox","stepsToReproduce":"eat his carrots", "_id":2},
-    {"title":"As Nodt's Tatar Foras","description":"As Nodt will make you feel intense fear","stepsToReproduce":"make him use his Vollstandig","_id":3}
+    {"title":"Bonzi Buddy","description":"Bonzi Buddy will pop up and sing Chanel","stepsToReproduce":"see on both sides like chanel","classification":"Munkey","classifiedOn":new Date(),"lastUpdated": new Date(),"assignedToUserId":1 ,"assignedToUserName":"Tyler","assignedOn":new Date(), "_id":1},
+    {"title":"Bugs Bunny","description":"Bugs will put a pipe bomb in your mailbox","stepsToReproduce":"eat his carrots","classification":"Bunnyboi","classifiedOn":new Date(),"lastUpdated": new Date(),"assignedToUserId": 8,"assignedToUserName":"Ada Wong ","assignedOn":new Date(), "_id":2},
+    {"title":"As Nodt's Tatar Foras","description":"As Nodt will make you feel intense fear","stepsToReproduce":"make him use his Vollstandig", "classification":"Sternritter","classifiedOn": new Date(),"lastUpdated": Date(),"assignedToUserId": 7 ,"assignedToUserName":"Leon Kennedy ","assignedOn":new Date(), "_id":3}
 ];
 
 router.get('/list', (req,res) => {
@@ -32,33 +32,101 @@ router.get('/:bugId', (req,res) =>{
 
 router.post('/new', (req,res) => {
     //FIXME:create a new bug and send response as JSON
-    const { title, description, stepsToReproduce } = req.body;
+    const { title, description, stepsToReproduce} = req.body;
+    const newBug = {title, description, stepsToReproduce} ;
+    if(title, description, stepsToReproduce)
+    {
+      const id = nanoid();
+      newBug._id = id;
+      bugsArray.push(newBug);
+      res.status(200).type('text/plain').send(`New Bug Reported!`);
+    }
+    if (!title || !description|| !stepsToReproduce )
+    {
+      return res.status(400).type('text/plain').send('Invalid data provided.');
+    };
 
-  // Check if any required field is missing or invalid
-  if (!title || !description || !stepsToReproduce) {
-    return res.status(400).type('text/plain').send('Invalid data provided.');
-  }
-  const newBug = {id: nanoid(), title, description, stepsToReproduce};
-
-  bugsArray.push(newBug);
-
-  res.status(200).type('text/plain').send('New bug reported!');
 });
 
 router.put('/:bugId',(req,res) => {
     //FIXME: update existing bug and send response as JSON
+    const bugId = req.params.bugId;
+  const currentBug = bugsArray.find(bug => bug._id == bugId);
+
+  //for this line to work, you have to have a body parser
+  const updatedBug = req.body;
+
+  if(currentBug){
+      for(const key in updatedBug){
+          if(currentBug[key] != updatedBug[key]){
+            currentBug[key] = updatedBug[key];
+          }
+      }
+
+      //save the currentBug into the array
+      const index = bugsArray.findIndex(bug => bug._id == bugId);
+      if(index != -1){
+        bugsArray[index] = currentBug;
+      }
+
+     res.status(200).type('text/plain').send(`Bug updated!`);
+  }else{
+      res.status(404).type('text/plain').send(`User ${bugId} not found`);
+  }
+  res.json(updatedBug);
 });
 
 router.put('/:bugId/classify',(req,res) =>{
     //FIXME: classify bug and send response as JSON
+    const bugId = req.params.bugId;
+    const { classification } = req.body;
+
+    if (!classification) {
+        return res.status(400).type('text/plain').send('Invalid data provided. Classification is missing.');
+    }
+
+    const bug = bugsArray.find((bug) => bug._id === bugId);
+
+    if (!bug) {
+        return res.status(404).type('text/plain').send(`Bug ${bugId} not found.`);
+    }
+
+    bug.classification = classification;
+    bug.classifiedOn = new Date();
+    bug.lastUpdated = new Date();
+
+    res.status(200).type('text/plain').send('Bug classified!');
+
 });
 
 router.put('/:bugId/assign', (req,res) =>{
     //FIXME: assign bug to a user and send response as JSON
+    const bugId = req.params.bugId;
+    const { assignedToUserId, assignedToUserName } = req.body;
+    const bug = bugsArray.find((bug) => bug._id === bugId);
+
+
+  // Check if assignedToUserId and assignedToUserName are missing or invalid
+  if (!assignedToUserId || !assignedToUserName) {
+    return res.status(400).type('text/plain').send('Invalid data provided. assignedToUserId and assignedToUserName are required.');
+  }
+
+  if (!bug) {
+    return res.status(404).type('text/plain').send(`Bug ${bugId} not found.`);
+  }
+
+  // Update the bug's assignedToUserId, assignedToUserName, assignedOn, and lastUpdated fields
+  bug.assignedToUserId = assignedToUserId;
+  bug.assignedToUserName = assignedToUserName;
+  bug.assignedOn = new Date();
+  bug.lastUpdated = new Date();
+
+  res.status(200).type('text/plain').send('Bug assigned!');
 });
 
 router.put("/:bugId/close", (req,res) =>{
     //FIXME: close bug and send response as JSON
+
 });
 
 export {router as BugRouter};
